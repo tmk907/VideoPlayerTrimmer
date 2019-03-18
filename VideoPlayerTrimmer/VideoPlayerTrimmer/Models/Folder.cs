35841cd4a55e23b7e5998f715c5cad8d90389a96
Tomasz Walecki <tomek_w@outlook.com>
2019-03-18 20:17:23 +0100

    Add Folder and VideoFile models

---
+++ VideoPlayerTrimmer/VideoPlayerTrimmer/Models/Folder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoPlayerTrimmer.Framework;
+
+namespace VideoPlayerTrimmer.Models
+{
+    public class Folder : BindableBase
+    {
+        public string FolderName { get; set; }
+
+        public string FolderPath { get; set; }
+
+        private bool areNewVideos;
+        public bool AreNewVideos
+        {
+            get { return areNewVideos; }
+            set { SetProperty(ref areNewVideos, value); }
+        }
+
+        private int videoCount;
+        public int VideoCount
+        {
+            get { return videoCount; }
+            set { SetProperty(ref videoCount, value); }
+        }
+    }
+}
---
+++ VideoPlayerTrimmer/VideoPlayerTrimmer/Models/VideoFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoPlayerTrimmer.Framework;
+
+namespace VideoPlayerTrimmer.Models
+{
+    public class VideoFile : BindableBase
+    {
+        public string FilePath { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FolderPath { get; set; }
+
+        private bool isNew;
+        public bool IsNew
+        {
+            get { return isNew; }
+            set { SetProperty(ref isNew, value); }
+        }
+
+        public TimeSpan Duration { get; set; }
+    }
+}
