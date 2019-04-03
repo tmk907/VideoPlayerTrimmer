d810290a586379886a73871b4c00fc64af5c9076
Tomasz Walecki <tomek_w@outlook.com>
2019-04-03 14:27:17 +0200

    FoldersPage display list of folders with videos

--- VideoPlayerTrimmer/VideoPlayerTrimmer/ViewModels/FoldersViewModel.cs
+++ VideoPlayerTrimmer/VideoPlayerTrimmer/ViewModels/FoldersViewModel.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using VideoPlayerTrimmer.Framework;
+using VideoPlayerTrimmer.Models;
+using VideoPlayerTrimmer.Services;
 
 namespace VideoPlayerTrimmer.ViewModels
 {
-    class FoldersViewModel
+    public class FoldersViewModel : BaseViewModel
     {
+        private readonly IVideoLibrary videoLibrary;
+
+        public FoldersViewModel(IVideoLibrary videoLibrary)
+        {
+            this.videoLibrary = videoLibrary;
+            Folders = new ObservableCollection<FolderItem>();
+            if (Xamarin.Forms.DesignMode.IsDesignModeEnabled)
+            {
+                Folders.Add(new FolderItem()
+                {
+                    FolderName = "Folder1"
+                });
+                Folders.Add(new FolderItem()
+                {
+                    FolderName = "Folder2"
+                });
+            }
+        }
+
+        public ObservableCollection<FolderItem> Folders { get; set; }
+
+        public override async Task InitializeAsync()
+        {
+            var list = await videoLibrary.GetFolderItemsAsync(true);
+            foreach (var item in list)
+            {
+                Folders.Add(item);
+            }
+        }
     }
 }
--- VideoPlayerTrimmer/VideoPlayerTrimmer/Views/FoldersPage.xaml.cs
+++ VideoPlayerTrimmer/VideoPlayerTrimmer/Views/FoldersPage.xaml.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using VideoPlayerTrimmer.Framework;
+using VideoPlayerTrimmer.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace VideoPlayerTrimmer.Views
 {
+    public class ForldersContentPage : BaseContentPage<FoldersViewModel> { }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class FoldersPage : ContentPage
+    public partial class FoldersPage : ForldersContentPage
     {
         public FoldersPage()
         {
             InitializeComponent();
+            BindingContext = App.Container.Resolve<FoldersViewModel>();
         }
     }
 }
