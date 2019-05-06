e229d0fff723ec490104832229462b3865e90094
Tomasz Walecki <tomek_w@outlook.com>
2019-05-06 13:33:13 +0200

    Add settings

---
+++ VideoPlayerTrimmer/VideoPlayerTrimmer/Services/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace VideoPlayerTrimmer.Services
+{
+    public class Settings
+    {
+        public static int VideoBrightness
+        {
+            get { return Preferences.Get(nameof(VideoBrightness), 6); }
+            set { Preferences.Set(nameof(VideoBrightness), value); }
+        }
+
+
+    }
+}
