eb2ba6c1983373f7f8f11692ab101931793936c9
Tomasz Walecki <tomek_w@outlook.com>
2019-05-23 22:10:02 +0200

    FavoriteScenesPage

--- VideoPlayerTrimmer/VideoPlayerTrimmer/ViewModels/FavoriteScenesViewModel.cs
+++ VideoPlayerTrimmer/VideoPlayerTrimmer/ViewModels/FavoriteScenesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using VideoPlayerTrimmer.Framework;
@@ -9,5 +10,43 @@ namespace VideoPlayerTrimmer.ViewModels
 {
     public class FavoriteScenesViewModel : BaseViewModel
     {
+        private readonly IVideoLibrary videoLibrary;
+
+        public FavoriteScenesViewModel(IVideoLibrary videoLibrary)
+        {
+            this.videoLibrary = videoLibrary;
+        }
+
+        public override async Task OnAppearingAsync(bool firstTime)
+        {
+            await LoadDataAsync();
+        }
+
+        private async Task LoadDataAsync()
+        {
+            GroupedFavoriteScenes.Clear();
+            var videos = await videoLibrary.GetVideoItemsAsync("", false);
+            foreach(var video in videos)
+            {
+                var favScenes = await videoLibrary.GetFavoriteScenes(video.VideoId);
+                var favList = new FavoriteList()
+                {
+                    VideoTitle = video.Title,
+                };
+                favList.AddRange(favScenes);
+                if (favList.Count > 0)
+                {
+                    GroupedFavoriteScenes.Add(favList);
+                }
+            }
+        }
+
+        public ObservableCollection<FavoriteList> GroupedFavoriteScenes { get; } = new ObservableCollection<FavoriteList>();
+    }
+
+    public class FavoriteList : List<FavoriteScene>
+    {
+        public string VideoTitle { get; set; }
+        public List<FavoriteScene> Favorites => this;
     }
 }
