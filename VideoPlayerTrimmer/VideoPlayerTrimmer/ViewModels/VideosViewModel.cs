973b7f526726386449f913c2ef25f47ee0725be5
Tomasz Walecki <tomek_w@outlook.com>
2019-11-16 16:41:47 +0100

    Use AsyncCommand

--- VideoPlayerTrimmer/VideoPlayerTrimmer/ViewModels/VideosViewModel.cs
+++ VideoPlayerTrimmer/VideoPlayerTrimmer/ViewModels/VideosViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AsyncAwaitBestPractices.MVVM;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -19,7 +20,7 @@ namespace VideoPlayerTrimmer.ViewModels
         {
             App.DebugLog("");
             this.videoLibrary = videoLibrary;
-            ItemTappedCommand = new Command<VideoItem>(async (item) => await NavigateToPlayerPage(item));
+            ItemTappedCommand = new AsyncCommand<object>((item) => NavigateToPlayerPage(item));
         }
 
         public string Directory { get; set; }
@@ -28,7 +29,7 @@ namespace VideoPlayerTrimmer.ViewModels
 
         public ObservableRangeCollection<VideoItem> VideoItems { get; set; } = new ObservableRangeCollection<VideoItem>();
 
-        public Command ItemTappedCommand { get; set; }
+        public IAsyncCommand<object> ItemTappedCommand { get; set; }
 
         public override void OnNavigating(Dictionary<string, string> navigationArgs)
         {
@@ -53,17 +54,18 @@ namespace VideoPlayerTrimmer.ViewModels
             VideoItems.AddRange(list.OrderBy(e => e.Title));
         }
 
-        public async Task NavigateToPlayerPage(VideoItem item)
+        public async Task NavigateToPlayerPage(object item)
         {
+            var videoItem = item as VideoItem;
             if (backToTrimmer)
             {
                 await App.NavigationService.NavigateToAsync($"{PageNames.TrimmerNav}" +
-                    $"?{NavigationParameterNames.VideoPath}={item.FilePath}");
+                    $"?{NavigationParameterNames.VideoPath}={videoItem.FilePath}");
             }
             else
             {
                 await App.NavigationService.NavigateToAsync($"{PageNames.Player}" +
-                    $"?{NavigationParameterNames.VideoPath}={item.FilePath}");
+                    $"?{NavigationParameterNames.VideoPath}={videoItem.FilePath}");
             }
         }
     }
